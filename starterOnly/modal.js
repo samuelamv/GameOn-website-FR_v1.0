c377809bdd8305b62138c97e1e296df5abc768a0
Samuel Martins Vieira <samuelalexandre.nov@gmail.com>
2024-03-03 15:39:06 +0100

    recupération des valeurs du formulaire d'inscription + fonction fermer le modal form

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,42 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalClose = document.querySelector(".close");
+
+let unPrenom = document.getElementById("first");
+console.log(unPrenom);
+let unNom = document.getElementById("last");
+console.log(unNom);
+let unEmail = document.getElementById("email");
+console.log(unEmail);
+let uneDateAnniversaire = document.getElementById("birthdate");
+console.log(uneDateAnniversaire);
+let nombreTournois = document.getElementById("quantity");
+console.log(nombreTournois);
+let lieuTournois = document.querySelectorAll(".formDate input");
+console.log(lieuTournois);
+let conditionUtilisation = document.getElementById("checkbox1");
+console.log(conditionUtilisation);
+let notifEvenements = document.getElementById("checkbox2");
+console.log(notifEvenements);
+let validationBtn = document.querySelector(".btn-submit");
+console.log(validationBtn);
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// close modal event
+modalClose.addEventListener("click", closeModal);
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
 
 
